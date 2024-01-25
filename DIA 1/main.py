fa847df166b39a3b615191b0d7b88a8b9ada10cf
Michelle Alvarez <michellzarethalvarez1124@gmail.com>
2024-01-25 09:35:29 -0500

    trabajo en clase

---
+++ DIA 1/main.py
@@ -0,0 +1,44 @@
+##----------------
+##-----ejercicio 1-------
+##-------------------
+
+#impresion  en consola
+print("hola mundo")
+
+#----datos primitivos------
+#1, string
+texto = "campus"
+print(texto)
+print(type(texto))
+#2, int
+numeroEntero = 1
+print(numeroEntero)
+#3, float
+numeroDecimal = 3.1
+print(numeroDecimal)
+#4, double
+numeroDecimalLargo = 3.14157582737283
+print(numeroDecimalLargo)
+#5, boolean
+booleano = True
+print(booleano)
+#----entradas parte del usuario----
+entradaUsuarionumero = input("ingresa tu edad:")
+numerofinal = int(entradaUsuarionumero)
+print ("tu edad es",numerofinal)
+#-----ciclos-----
+#ciclo for
+for i in range (5,10,2): #for contador in range (desde,hasta,pasos):
+    
+    print (i)
+#ciclo while
+booleanito = True;#while condicion a cumplir;
+print("sigo vivo")
+booleanito = False
+#------condicionales--------
+texto1 = "campus"
+if texto1 == "campus":
+    print("soy campus")
+else:
+    print ("no soy campus")
+## desarrollado por MICHELLE ZARETH ALVAREZ PULIDO - 1099738508
